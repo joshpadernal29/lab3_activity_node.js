1f3f4366d16af961903a85890d3c6123a8f815d1
joshpadernal29 <josh.padernal29@gmail.com>
2026-02-27 01:47:35 +0800

    added the item for getting and dispalying items inputted from user

--- index.js
+++ index.js
@@ -19,10 +19,40 @@ app.get('/about', (req, res) => {
 // post request
 app.use(express.json()); // parse json bodies
 
-app.post('/submit', (req,res) => {
+app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Data Recieved: ${JSON.stringify(data)}`);
 });
+
+
+// route for displaing a list of items
+const items = ['apple', 'bannana', 'orange'];
+// get items
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+// post items
+app.post('/items', (req, res) => {
+    const newItem = req.body.item; // store the data from the input
+    console.log("Adding new item:", newItem);
+    items.push(newItem); // add the input item to the items list
+    res.json(items);
+});
+
+
+
+//  testing  the error handler
+app.get('/test-error', (req, res) => {
+    throw new Error("");
+});
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
 
 // start server
 app.listen(port, () => {
